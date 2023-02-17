19ab62a9fa0e4c379f7d139ed0c60a6e2e880593
Igbobi williams <dabs.collection@gmail.com>
2023-02-17 12:50:09 -0800

    betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  * Main - Entry point
- * Description: 'Writing C'
+ * Description - writing C
  * Return: 0
- */
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
