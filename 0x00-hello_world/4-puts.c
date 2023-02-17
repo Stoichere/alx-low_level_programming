244086ab7ee214f6e5fd4eff1b610d67c25b1299
Igbobi williams <dabs.collection@gmail.com>
2023-02-17 11:38:18 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc $CFILE -o cisfun
+#include <stdio.h>
+/**
+ *  main -  main block
+ *  return: 0
+ */
+ int main(void)
+ {
+ 	puts("\"Programming is like building a multilingual puzzle");
+  	return(0);
+}
