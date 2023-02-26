ec07a29e00d8437a4c3da059c2910b9e7908ec28
Igbobi williams <dabs.collection@gmail.com>
2023-02-25 23:05:13 -0800

    quesiton five

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description - question five
+ * Return: (0)
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = '0'; alp <= '9'; ++alp)
+	{
+		putchar(alp);
+	}
+	putchar('\n');
+	return (0);
+}
